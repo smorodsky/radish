cc793bced26d43e4844c2e65285e26846e5c2420
smorodsky <smorodskiy@kosmo.local>
2012-12-21 17:58:41 +0400

    Convert to extention

---
+++ src/Startup Scripts/Radish_Launcher.jsx
@@ -0,0 +1,14 @@
+﻿// 2012-12-21
+
+var file = new File(app.activeScript.parent.parent.absoluteURI + '/Radish/Radish.jsx');
+
+
+if (!file.exists) {
+	file = new File(app.activeScript.parent.parent.absoluteURI + '/Radish.jsx');
+}
+
+if (file.exists) {
+	app.doScript(file, ScriptLanguage.JAVASCRIPT);
+} else {
+	alert('Radish\rVersion control does not work.\rFile "' + file.fsName + '" not found.');
+}
